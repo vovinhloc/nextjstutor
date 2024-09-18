510f5af1d85fd6730cbdbcf65f11fe6585ba503d
vovinhloc <vovinhl@yahoo.com>
2024-09-18 10:48:37 +0700

    final

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,4 +1,9 @@
 import React from 'react'
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Locvv Customer | Acme Dashboard',
+};
 
 export default function DashboardCustomersPage() {
   return (
